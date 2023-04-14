83a432e46ce8cd3adff0d870a91fb1e8ce853f73
keatshackon <45368256+keatshackon@users.noreply.github.com>
2023-04-14 11:02:48 +0530

    Time: 260 ms (16.44%), Space: 73 MB (56.30%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -23,14 +23,38 @@ public:
         
         string t = s;
         
-        reverse(begin(t),end(t));
-        
+        reverse(begin(t),end(t)); 
         int n = s.length();
         
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        //vector<vector<int>>dp(n,vector<int>(n,-1));
+        //return solve(0,0,s,t,dp);
+            
+        
+        vector<vector<int>>dp2(n+1,vector<int>(n+1,0));
+        
+        for(int i = 1;i <= n;i++){
+            for(int j = 1;j <= n;j++){
+                if(s[i-1] == t[j-1]){
+                    dp2[i][j] = 1 + dp2[i-1][j-1];
+                }else{
+                    int s1 = 0;
+                    int s2 = 0;
+                    
+                    s1 = dp2[i-1][j];
+                    s2 = dp2[i][j-1];
+                            
+                    dp2[i][j] = max(s1,s2);
+                }
+            }
+        }
         
         
-        return solve(0,0,s,t,dp);
+        return dp2[n][n];
+        
+        
+        
+        
+        
     
     }
 };
