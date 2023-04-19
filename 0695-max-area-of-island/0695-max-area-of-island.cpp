e3be38683a9ed7268a7dcc148d8b3307ac921f17
keatshackon <45368256+keatshackon@users.noreply.github.com>
2023-04-19 16:14:36 +0530

    Time: 24 ms (44.97%), Space: 24 MB (40.51%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    void dfs(int x,int y,vector<vector<int>>&grid,vector<vector<int>>&vis,int &cnt){
+    void solve(int x,int y,int &cnt,vector<vector<int>>&grid,vector<vector<int>>&vis){
         
         int row = grid.size();
         int col = grid[0].size();
@@ -13,38 +13,40 @@ public:
             int new_x = x + dx[i];
             int new_y = y + dy[i];
             
-            if(new_x >= 0 and new_x < row and new_y >=0  and new_y < col and grid[new_x][new_y] == 1
-               and vis[new_x][new_y] == 0){
+            if(new_x >= 0 and new_x < row and new_y >= 0 and new_y < col and grid[new_x][new_y] == 1 
+               and !vis[new_x][new_y]){
                 
                 cnt++;
                 vis[new_x][new_y] = 1;
-                dfs(new_x,new_y,grid,vis,cnt);
+                solve(new_x,new_y,cnt,grid,vis);
             }
         }
     }
     
+    
+    
     int maxAreaOfIsland(vector<vector<int>>& grid) {
+        
         
         int row = grid.size();
         int col = grid[0].size();
         
         vector<vector<int>>vis(row,vector<int>(col,0));
-        
+    
         int ans = 0;
-        int cnt = 0;
         
         for(int i = 0;i < row;i++){
             for(int j = 0;j < col;j++){
-                if(grid[i][j] == 1 and vis[i][j] == 0){
+                if(grid[i][j] == 1 and !vis[i][j]){
+                    int cnt = 0;
                     vis[i][j] = 1;
                     cnt++;
-                    dfs(i,j,grid,vis,cnt);
-                    ans = max(cnt,ans);
-                    cnt = 0;
+                    solve(i,j,cnt,grid,vis);
+                    ans = max(ans,cnt);
                 }
             }
         }
-        
         return ans;
+       
     }
 };
