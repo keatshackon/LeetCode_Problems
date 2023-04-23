4366f19516c2d7642b6da073f0cccb87b9239c9d
keatshackon <45368256+keatshackon@users.noreply.github.com>
2023-04-23 06:50:34 +0530

    Time: 11 ms (83.24%), Space: 15.7 MB (61.74%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -7,7 +7,8 @@ public:
         int j = numbers.size()-1;
         
         while(i < j){
-            if(numbers[i] + numbers[j] == target){
+            
+            if(numbers[i]+numbers[j] == target){
                 return {i+1,j+1};
             }else if(numbers[i]+numbers[j] < target){
                 i++;
