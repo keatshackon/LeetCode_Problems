3264460ea3d834bbac5e1b30018fbd8ac8d2006a
keatshackon <45368256+keatshackon@users.noreply.github.com>
2023-04-25 07:23:47 +0530

    Time: 0 ms (100.00%), Space: 6 MB (35.83%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        
+        int cnt = 0;
+        for(int i = 0;i <= 31;i++){
+            if(n & 1 == 1){
+                cnt++;
+            }
+            n = n>>1;
+        }
+        return cnt;
+    }
+};
