59efe0ab3dab7e4774535b6e5def87bd709757a0
keatshackon <45368256+keatshackon@users.noreply.github.com>
2023-04-29 09:22:15 +0530

    Time: 1280 ms (8.56%), Space: 51.3 MB (8.74%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    bool solve(int idx,int n,vector<int>&nums,vector<int>&dp){
+        
+        if(idx == (n-1)){
+            return true;
+        }else if(idx >= n){
+            return false;
+        }
+        
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+        
+        int k = nums[idx];
+        bool s1 = false;
+        for(int i = 1;i <= k;i++){
+            s1 = solve(idx+i,n,nums,dp);
+            if(s1) break;
+        }
+        
+        return dp[idx] = s1;
+    }
+    
+    bool canJump(vector<int>& nums) {
+        
+        
+        int n = nums.size();
+        
+        vector<int>dp(n,-1);
+    
+    
+        bool ans =  solve(0,n,nums,dp);
+        
+        for(auto q:dp){
+            cout<<q<<" ";
+        }
+        cout<<"\n";
+        
+        return ans; 
+        
+        
+        
+    }
+};
