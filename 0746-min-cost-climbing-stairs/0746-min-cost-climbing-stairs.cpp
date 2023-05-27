781c0697e1af7bfff4c744bb7572ff5585c1780b
keatshackon <45368256+keatshackon@users.noreply.github.com>
2023-05-27 09:58:09 +0530

    Time: 3 ms (93.93%), Space: 13.7 MB (49.54%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    
+    int solve(int idx,vector<int>&c,vector<int>&dp){
+        
+        if(idx >= c.size()){
+            return 0;
+        }
+        
+        if(dp[idx] != -1) return dp[idx];
+        
+        int s1 = c[idx] + solve(idx+1,c,dp);
+        
+        int s2 = c[idx] + solve(idx+2,c,dp);
+        
+        return dp[idx] = min(s2,s1);
+    }
+    
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        int n  = cost.size();
+        vector<int>dp(n,-1);
+        
+        
+        solve(0,cost,dp);
+        
+        return min(dp[0],dp[1]);
+        
+        
+        
+        
+//         dp[0] = cost[0];
+//         dp[1] = min(cost[0],cost[1]);
+        
+//         for(int i = 2 ;i <= n;i++){
+            
+//             int s1 = c[idx] + solve(idx+1,c,dp);
+//             int s2 = c[idx] + solve(idx+2,c,dp);
+
+//             dp[idx] = min(s2,s1);
+//         }
+        
+//         for(auto q:dp){
+//             cout<<q<<" ";
+//         }
+        
+//         return dp[n];
+    }
+};
